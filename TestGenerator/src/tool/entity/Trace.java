404e5b109e5e39aaed5b0928a8f4f3be0f5151b3
hetao <tao.he1989@gmail.com>
2013-12-23 20:37:46 +0800

    init

---
+++ TestGenerator/src/tool/entity/Trace.java
@@ -0,0 +1,51 @@
+package tool.entity;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+public class Trace {
+
+	private List<Node> nodes;
+	private String loops;
+	
+	public List<Node> getNodes() {
+		return nodes;
+	}
+
+	public void setNodes(List<Node> nodes) {
+		this.nodes = nodes;
+	}
+
+	public String getLoops() {
+		return loops;
+	}
+
+	public void setLoops(String loops) {
+		this.loops = loops;
+	}
+
+	public class Node {
+		private String stateId;
+		private Map<String, String> stateVars;
+		
+		public String getStateId() {
+			return stateId;
+		}
+		public void setStateId(String stateId) {
+			this.stateId = stateId;
+		}
+		public Map<String, String> getStateVars() {
+			return stateVars;
+		}
+		public void setStateVars(Map<String, String> stateVars) {
+			this.stateVars = stateVars;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return new Gson().toJson(this);
+	}
+}
