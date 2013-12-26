726455f42f862ecb7fa42ed45f78068b760c94af
hetao <tao.he1989@gmail.com>
2013-12-26 19:12:08 +0800

    增加解析uml状态图中的初始状态

--- TestGenerator/src/StateChart/xmlparser/StateChartParser.java
+++ TestGenerator/src/StateChart/xmlparser/StateChartParser.java
@@ -16,6 +16,8 @@ import StateChart.entity.State;
 import StateChart.entity.Transition;
 import StateChart.entity.Trigger;
 import StateChart.entity.UMLStateChart;
+import StateChart.exception.StateChartException;
+import StateChart.exception.StateChartExceptionCode;
 
 public class StateChartParser {
     
@@ -33,21 +35,18 @@ public class StateChartParser {
         } 
     }
 
-    @SuppressWarnings("unchecked")
     public static UMLStateChart parser() throws Exception{
-        List<Element> usmNodes = doc.selectNodes("//UML:StateMachine");
-        if(null == usmNodes || usmNodes.size() > 1) {
+        Element usm = (Element) doc.selectSingleNode("//UML:StateMachine");
+        if(null == usm) {
             return null;
         }
-        
-        Element usmEl = (Element)usmNodes.get(0);
         UMLStateChart usc = new UMLStateChart();
-        usc.setId(usmEl.attributeValue("xmi.id"));
-        usc.setName(usmEl.attributeValue("name"));
-        
+        usc.setId(usm.attributeValue("xmi.id"));
+        usc.setName(usm.attributeValue("name"));
         usc.setTriggers(parseTriggers());
         usc.setStates(parseStates());
         usc.setTransitions(parseTransitions(usc));
+        usc.setInitState(usc.getStateById(parseInitState(usm)));
         
         return usc;
     }
@@ -113,13 +112,52 @@ public class StateChartParser {
     }
     
     /**
+     * 1.找到在最外层的开始状态（即整个状态图的Pseudostate，而非组合状态内的Pseudostate）
+     * 2.找到引用到它的迁移（只有一个）
+     * 3.取出该迁移的target，获得初始状态，如果该状态是组合状态取出组合状态中的第一个状态，如果是简单状态不须做处理
+     */
+    @SuppressWarnings("unchecked")
+    private static String parseInitState(Element usm) {
+        
+        List<Node> nodes = usm.selectNodes("UML:StateMachine.top/UML:CompositeState/UML:CompositeState.subvertex/UML:Pseudostate[@xmi.id]");
+        
+        if(null == nodes || nodes.size() == 0) {
+            throw new StateChartException(StateChartExceptionCode.NO_INIT_STATE, "状态图没有初始化状态");
+        }
+        
+        if(nodes.size() > 1) {
+            throw new StateChartException(StateChartExceptionCode.MULTI_INIT_STATE, "状态图多个初始状态");
+        }
+        
+        Element el = (Element) nodes.get(0);
+        List<Node> pseudoRefNodes = usm.selectNodes(".//UML:Pseudostate[@xmi.idref='"+el.attributeValue("xmi.id")+"']");
+        if(null == pseudoRefNodes || pseudoRefNodes.size() == 0) {
+            throw new StateChartException(StateChartExceptionCode.NO_TRANSITION_INIT_STATE, "状态图初始状态没有迁移");
+        }
+        if(pseudoRefNodes.size() > 1) {
+            throw new StateChartException(StateChartExceptionCode.MULTI_TRANSITION_INIT_STATE, "状态图初始状态包含多个迁移");
+        }
+        
+        List<Element> targetStates = pseudoRefNodes.get(0).selectNodes("../../UML:Transition.target/*");
+        if(null == targetStates ||targetStates.size() != 1) {
+            throw  new StateChartException(StateChartExceptionCode.XMI_FORMAT_ERROR, "xmi格式错误，开始状态target没有或者有多个");
+        }
+        
+        if("UML:CompositeState".equals(targetStates.get(0).getName())) {
+            return getFirstStateIdInCompositeState(targetStates.get(0));
+        }else {
+            return targetStates.get(0).attributeValue("xmi.idref");
+        }
+    }
+    
+    /**
      * 返回组合状态中的初始子状态id
      * @param 引用组合状态的节点  eg.<UML:CompositeState xmi.idref = '111--70-113-77--5c58af58:1431fb405ae:-8000:0000000000000965'/>
      */
-    private static String getFirstStateIdInCompositeState(Node node) throws Exception{
+    private static String getFirstStateIdInCompositeState(Node node){
         
         if(null == node) {
-            throw new Exception("参数不允许为null");
+            throw new StateChartException(StateChartExceptionCode.METHOD_PARAM_ERROR, "参数不允许为null");
         }
         
         Element e = (Element) node;
@@ -130,12 +168,13 @@ public class StateChartParser {
             Element compositeStateEl = (Element) doc.selectSingleNode("//UML:CompositeState[@xmi.id='" + e.attributeValue("xmi.idref") + "']");
             Element cStateOutTransRefEl = (Element) compositeStateEl.selectSingleNode("UML:CompositeState.subvertex/UML:Pseudostate/UML:StateVertex.outgoing/UML:Transition");
             if(null == cStateOutTransRefEl) {
-                throw new Exception("xml格式有问题，组合状态中没有Pseudostate状态");
+                throw  new StateChartException(StateChartExceptionCode.XMI_FORMAT_ERROR, "xml格式有问题，组合状态中没有Pseudostate状态");
+                
             }
             Element firstStateEl = (Element) doc.selectSingleNode("//UML:Transition[@xmi.id='"+cStateOutTransRefEl.attributeValue("xmi.idref")+"']/UML:Transition.target/UML:SimpleState");
             return firstStateEl.attributeValue("xmi.idref");
         }else {
-            throw new Exception("xml格式有问题");
+            throw  new StateChartException(StateChartExceptionCode.METHOD_PARAM_ERROR, "参数node不是SimpleState或者CompositeState");
         }
     } 
     
